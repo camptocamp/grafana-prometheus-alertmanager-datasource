c029c8b64a081894e781a4d3e9161e309649990c
BIOCATCH\Leon.Karabchevsky <leon.karabchevsky@biocatch.com>
2018-04-16 20:42:38 +0300

    alert sources with different lables & annotations can be displayed togather

--- src/datasource.js
+++ src/datasource.js
@@ -16,109 +16,128 @@ export class GenericDatasource {
     this.templateSrv = templateSrv;
   }
 
-  query(options) {
-    var query = this.buildQueryParameters(options);
-    query.targets = query.targets.filter(t => !t.hide);
+    query(options) {
+        let query = this.buildQueryParameters(options);
+        query.targets = query.targets.filter(t => !t.hide);
 
-    if (query.targets.length <= 0) {
-      return this.q.when({data: []});
-    }
-    // Format data for table panel
-    if(query.targets[0].type === "table"){
-      let filter = encodeURIComponent(this.templateSrv.replace(query.targets[0].expr, options.scopedVars) || "");
-      return this.backendSrv.datasourceRequest({
-        url: this.url + '/api/v1/alerts?silenced=false&inhibited=false&filter='+filter,
-        data: query,
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
-      }).then(response => {
-          let results = {
-              "data": [{
-                  "rows": [],
-                  "columns": [],
-                  "type": "table"
-              }]
-          };
-
-          if(response.data && response.data.data && response.data.data.length) {
-              let severity = response.data.data[0].labels.severity;
-              delete response.data.data[0].labels.severity;
-              results.data[0].columns = this.getColumns(response.data.data[0]);
-              for (let i = 0; i < response.data.data.length; i++) {
-                  let item = response.data.data[i];
-                  delete item.labels.severity;
-                  let labelValues = Object.values(item.labels);
-                  let annotationValues = Object.values(item.annotations);
-                  let row = [Date.parse(item.startsAt)].concat(labelValues).concat(annotationValues);
-                  row.push([this.severityLevels[severity]]);
-                  results.data[0].rows.push(row);
-              }
-          }
-          return results;
-      });
-    }else{
-      let filter = encodeURIComponent(this.templateSrv.replace(query.targets[0].expr, options.scopedVars) || "");
-      return this.backendSrv.datasourceRequest({
-        url: this.url + '/api/v1/alerts?silenced=false&inhibited=false&filter='+filter,
-        data: query,
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
-      }).then(response => {
-        return {
-          "data": [
-            {
-              "datapoints": [
-                [response.data.data.length, Date.now()]
-              ]
-            }
-          ]
+        if (query.targets.length <= 0) {
+            return this.q.when({data: []});
         }
-      });
-    }
-  }
+        // Format data for table panel
+        if(query.targets[0].type === "table"){
+            let filter = encodeURIComponent(this.templateSrv.replace(query.targets[0].expr, options.scopedVars) || "");
+            return this.backendSrv.datasourceRequest({
+                    url: this.url + '/api/v1/alerts?silenced=false&inhibited=false&filter='+filter,
+                    data: query,
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json' }
+                }).then(response => {
+                    let results = {
+                        "data": [{
+                            "rows": [],
+                            "columns": [],
+                            "type": "table"
+                            }
+                        ]
+                    };
 
-    getColumns(dataRow) {
+                if(response.data && response.data.data && response.data.data.length) {
+                    let columnsDict = this.getColumnsDict(response.data.data);
+                    results.data[0].columns = this.getColumns(columnsDict);
+
+                    for (let i = 0; i < response.data.data.length; i++) {
+                        let row = new Array(results.data[0].columns.length).fill("");
+                        let item = response.data.data[i];
+                        row[0] = [Date.parse(item['startsAt'])];
+
+                        for (let label of Object.keys(item['labels'])) {
+                            if(label in columnsDict) {
+                                row[columnsDict[label]] = item['labels'][label];
+                            }
+                        }
+                        for (let annotation of Object.keys(item['annotations'])) {
+                            if(annotation in columnsDict) {
+                                row[columnsDict[annotation]] = item['annotations'][annotation];
+                            }
+                        }
+                        results.data[0].rows.push(row);
+                    }
+                }
+                return results;
+            });
+        } else {
+            let filter = encodeURIComponent(this.templateSrv.replace(query.targets[0].expr, options.scopedVars) || "");
+                return this.backendSrv.datasourceRequest({
+                url: this.url + '/api/v1/alerts?silenced=false&inhibited=false&filter='+filter,
+                data: query,
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' }
+            }).then(response => {
+                return {
+                    "data": [{ "datapoints": [ [response.data.data.length, Date.now()] ]}]
+                }
+            });
+        }
+    }
+
+    getColumns(columnsDict) {
         let columns =  [{ text: "Time", type: "time" }];
-        for(let label of Object.keys(dataRow.labels)) {
-            columns.push({ text: label, type: "string" })
+        for(let column of Object.keys(columnsDict)) {
+            columns.push({ text: column, type: "string" })
         }
-        for(let annotation of Object.keys(dataRow['annotations'])) {
-            columns.push({ text: annotation, type: "string" })
-        }
-        columns.push({ text: "severity", type: "string" });
         return columns;
     }
 
-  testDatasource() {
-    return this.backendSrv.datasourceRequest({
-      url: this.url + '/api/v1/status',
-      method: 'GET'
-    }).then(response => {
-      if (response.status === 200) {
-        return { status: "success", message: "Data source is working", title: "Success" };
+    // Creates a column index dictionary in to assist in data row construction
+    getColumnsDict(data) {
+        let index = 1; // 0 is the data column
+        let columnsDict = {};
+        for (let i = 0; i < data.length; i++) {
+            for (let label of Object.keys(data[i]['labels'])) {
+                if(!(label in columnsDict) && label !== 'severity') {
+                    columnsDict[label] = index++;
+                }
+            }
+            for (let annotation of Object.keys(data[i]['annotations'])) {
+                if(!(annotation in columnsDict)) {
+                    columnsDict[annotation] = index++;
+                }
+            }
+        }
+        columnsDict['severity'] = index;
+        return columnsDict;
+    }
+
+    testDatasource() {
+        return this.backendSrv.datasourceRequest({
+            url: this.url + '/api/v1/status',
+            method: 'GET'
+        }).then(response => {
+            if (response.status === 200) {
+                return { status: "success", message: "Data source is working", title: "Success" };
+            }
+        });
+    }
+
+    buildQueryParameters(options) {
+        //remove placeholder targets
+          options.targets = _.filter(options.targets, target => {
+          return target.target !== 'select metric';
+        });
+          options.targetss = _.map(options.targets, target => {
+          return {
+            target: this.templateSrv.replace(target.target),
+            expr: target.expr,
+            refId: target.refId,
+            hide: target.hide,
+            type: target.type || 'single',
+            legendFormat: target.legendFormat || ""
+          };
+        });
+        return options;
       }
-    });
-  }
 
-  buildQueryParameters(options) {
-    //remove placeholder targets
-      options.targets = _.filter(options.targets, target => {
-      return target.target !== 'select metric';
-    });
-      options.targetss = _.map(options.targets, target => {
-      return {
-        target: this.templateSrv.replace(target.target),
-        expr: target.expr,
-        refId: target.refId,
-        hide: target.hide,
-        type: target.type || 'single',
-        legendFormat: target.legendFormat || ""
-      };
-    });
-    return options;
-  }
-
-  formatInstanceText(labels, legendFormat){
+    formatInstanceText(labels, legendFormat){
     if(legendFormat === ""){
       return JSON.stringify(labels);
     }

22ecfa516dc45e6269250f403800cc2b3ec8bad5
Hugo Bollon <hugo.bollon@camptocamp.com>
2026-02-17 15:23:18 +0100

    feat(config): migrate ConfigEditor to @grafana/plugin-ui components

--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -1,20 +1,43 @@
 import React from 'react';
-import { DataSourceHttpSettings } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
+import {
+  ConnectionSettings,
+  Auth,
+  AdvancedHttpSettings,
+  ConfigSection,
+  convertLegacyAuthProps,
+} from '@grafana/plugin-ui';
 import { GenericOptions } from '../types';
 
 interface Props extends DataSourcePluginOptionsEditorProps<GenericOptions> {}
 
 export function ConfigEditor(props: Props) {
   const { onOptionsChange, options } = props;
+
   return (
-    <div className="gf-form-group">
-      <DataSourceHttpSettings
-        defaultUrl={'http://localhost:8080'}
-        dataSourceConfig={options}
-        showAccessOptions={true}
+    <>
+      <ConnectionSettings
+        config={options}
         onChange={onOptionsChange}
+        urlPlaceholder="http://localhost:8080"
+        description="Configure the connection to your Prometheus Alertmanager instance"
       />
-    </div>
+
+      <Auth
+        {...convertLegacyAuthProps({
+          config: options,
+          onChange: onOptionsChange,
+        })}
+      />
+
+      <ConfigSection
+        title="Advanced settings"
+        description="Additional HTTP settings for fine-tuning the connection"
+        isCollapsible
+        isInitiallyOpen={false}
+      >
+        <AdvancedHttpSettings config={options} onChange={onOptionsChange} />
+      </ConfigSection>
+    </>
   );
 }
